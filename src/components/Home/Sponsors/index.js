48341fc842fe05e9e75e2fe578e3a5a5c64c34b9
Ahmad44452 <88369569+Ahmad44452@users.noreply.github.com>
2022-10-07 20:41:07 +0500

    GDSC

--- src/components/Home/Sponsors/index.js
+++ src/components/Home/Sponsors/index.js
@@ -1,10 +1,10 @@
-
+import { motion } from 'framer-motion';
 
 const Sponsors = () => {
   return (
     <div className="sponsors__wrap" id="sponsors">
       <h1 className="sponsors__heading">Sponsors</h1>
-      <div className="sponsors">
+      <motion.div className="sponsors" initial={{ x: '100%' }} animate={{ x: '-100%' }} transition={{ repeat: Infinity, duration: 30, ease: "linear", repeatType: "reverse" }}>
         <img src="https://i.imgur.com/U6FxoV6.png" alt="microsoft" />
         <img src='https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png' alt="github" />
         <img src="https://i.imgur.com/Xzu1ynM.png" alt="meta school" />
@@ -24,7 +24,7 @@ const Sponsors = () => {
         <img src="https://i.imgur.com/U6FxoV6.png" alt="microsoft" />
         <img src='https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png' alt="github" />
         <img src="https://i.imgur.com/Xzu1ynM.png" alt="meta school" />
-      </div>
+      </motion.div>
     </div>
 
   )
